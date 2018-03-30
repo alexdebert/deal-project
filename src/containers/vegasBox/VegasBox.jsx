a3ef688d8ac3cfa02e360982b5c770b5fabe0ca8
Alexandre Debert <alexandre10.debert@gmail.com>
2018-03-29 23:54:10 -0300

    Untrack .idea files - Modifying Vegas Box List styles

--- src/containers/vegasBox/VegasBox.jsx
+++ src/containers/vegasBox/VegasBox.jsx
@@ -12,32 +12,32 @@ class VegasBox extends Component {
 	constructor() {
 		super()
 		this.state = {
-			isDisplayed: true
+			visible: true
 		}
 	}
 
 	toggleList() {
 		this.setState(prevState => ({
-			isDisplayed: !prevState.isDisplayed
+			visible: !prevState.visible
 		}))
 	}
 
 	render () {
-		let hiddenClass = this.state.isDisplayed ? '' : 'hidden',
-			classBox = `vegas-box-list-wrapper ${hiddenClass}`
+		let hidden = this.state.visible ? '' : 'hidden',
+			vegasBoxListClass = `vegas-box-list ${hidden}`
 
 		return (
 			<div id="vegas-box-wrapper">
 				<div onClick={() => this.toggleList()} className="vegas-box-headline">
 					<h2>Handcrafted Vegas Get Away</h2>
 				</div>
-				<div className={classBox}>
+				<div className={vegasBoxListClass}>
 					<ol>
 						<li>Describe your dream weekend</li>
 						<li>We match you with 1-2 top Vegas specialists who are the best fit. They work together to plan your custom itinerary.</li>
 						<li>{"Book the itinerary when you're satisfied"}</li>
 					</ol>
-					<button>
+					<button className="vegas-box-button">
 						Get Started Now
 					</button>
 				</div>
